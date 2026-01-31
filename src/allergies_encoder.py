1cffab6e40111b113c38daf6f5caf4edb2c27623
Povilas Sauciuvienas <p.sauciuvienas@gmail.com>
2026-01-31 23:31:10 +0000

    scoping allergies encoder working

--- src/allergies_encoder.py
+++ src/allergies_encoder.py
@@ -1,10 +1,21 @@
 import numpy
 import pandas as pd
+from typing import Literal
 
 class AllergiesEncoder:
-    def __init__(self):
-        allergy_df = pd.read_csv('data/allergies.csv')
+    def __init__(self, which_set:Literal['main', 'extended']='main'):
+        if which_set == 'main':
+            allergy_df = pd.read_csv('data/main_allergies.csv')
+        elif which_set == 'extended':
+            allergy_df = pd.read_csv('data/extended_allergies.csv')
+        else:
+            raise ValueError("which_set must be either 'main' or 'extended'")
+        
         self.allergen_list = allergy_df['allergen'].tolist()
+
+    @staticmethod
+    def lowercase_list(items:list[str])->list[str]:
+        return [item.lower() for item in items]
 
     def encode_allergy(self, allergies:list[str])->int:
         encoding = 0
@@ -22,4 +33,13 @@ class AllergiesEncoder:
         
         return allergies
     
-    def encode_allergies_batch(self, allergies_batch:list[list[str]])->numpy.ndarray:
+
+if __name__ == "__main__":
+    encoder = AllergiesEncoder()
+    test_allergies = ['cereals containing gluten']
+    encoded = encoder.encode_allergy(test_allergies)
+    print(f"Encoded: {encoded}")
+    decoded = encoder.decode_allergy(encoded)
+    print(f"Decoded: {decoded}")
+    
+
